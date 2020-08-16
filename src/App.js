9b6c9305ec95edc2281cccf96bb10d5024bb6414
salihcodev <ahmedsalihdev@gmail.com>
2020-08-16 11:46:15 +0200

    Intial commit and project starter point

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// ==> BASIC:
+import React from 'react';
+
+// ==> UTILITIES:
+import './styles/app.sass';
+
+// ==> vintage ROOT APP:
+export default function App() {
+  return <h1>hello from app component</h1>;
+}
