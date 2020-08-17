91a01371ff9abff3f630d189868d6995d980d2ec
salihcodev <ahmedsalihdev@gmail.com>
2020-08-18 01:46:44 +0200

    Finish <HeroComponent> - Mixins file added to styles.

--- src/components/hero/Hero.Component.jsx
+++ src/components/hero/Hero.Component.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+// ==> BASIC:
+import React from 'react';
+import './Hero.style.sass';
 
-export default function Hero() {
-  return <h1>hello from hero</h1>;
-}
+// ==> UTILITIES:
+import { Container } from 'react-bootstrap';
+
+// ==> COMPONENT DATA:
+const HeroHeading = 'Think, Code, Deploy';
+const HeroDescription = 'Embrace your choices  --we do';
+
+// ==> COMPONENT:
+const Hero = ({ children }) => {
+  return (
+    <>
+      <div className="hero">
+        <div className="heroContainer">
+          <Container>
+            <h1 className="homeHeroHeading">{HeroHeading}</h1>
+            <p className="homeHeroDescription">{HeroDescription}</p>
+
+            {children}
+          </Container>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
