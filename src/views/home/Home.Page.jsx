e12d763e34b5dabe44563f19bd7dde8febbd3745
Ahmed Salih <45488156+salihcodev@users.noreply.github.com>
2020-08-18 12:28:01 +0200

    Finish <HeroComponent> (#5)
    - Mixins file added to styles.

--- src/views/home/Home.Page.jsx
+++ src/views/home/Home.Page.jsx
@@ -1,10 +1,27 @@
+// ==> BASIC:
 import React from 'react';
 
+// ==> UTILITIES:
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+// ==> COMPONENTS:
+import Hero from '../../components/hero/Hero.Component';
+
+// ==> VIEWER:
 const HomePage = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <>
+      <section className="homePageViewer">
+        <Hero>
+          <Link to="/products">
+            <Button type="button" className="HomeHeroToProducts">
+              Browse to products
+            </Button>
+          </Link>
+        </Hero>
+      </section>
+    </>
   );
 };
 
